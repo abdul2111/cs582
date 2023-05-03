4318a09d112ecdb4255b52cb94e26141c46f8c2d
abdul2111 <105629025+abdul2111@users.noreply.github.com>
2023-05-03 14:31:08 -0500

    Add files via upload

--- abdul/pages/views.py
+++ abdul/pages/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 # Create your views here.
 def home_view(request, *args, **kwargs):
     print(request.user)
-    return HttpResponse("<h1>Hello World</h1>")
+    return render(request,"home.html",{})
 def contact_view(request, *args, **kwargs):
-    return HttpResponse("<h1>contact page</h1>")
+    print(request.user)
+    return render(request,"contact.html",{})
